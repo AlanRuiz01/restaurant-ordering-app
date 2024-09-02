59805b83d4901a7b07050269392c2ffda4376460
Alan Ruiz <matiasruizc315@gmail.com>
2024-09-02 15:46:32 -0400

    first changes

---
+++ index.js
@@ -0,0 +1,19 @@
+import {menuArray} from '/data.js'
+
+const itemsHtml = menuArray.map(function(food) {
+    return `
+    <section>
+    <div class="foods">
+        <i class="fa-solid fa-plus buy-icon"></i>
+        <p class="icon"> ${food.emoji} </p>
+            <div class="details">
+                <p class="food-names">${food.name}</p>
+                <p class="food-ingredients">${food.ingredients}</p>
+                <p class="price">$${food.price}</p>
+            </div>
+    </div>
+    </section>
+    `;
+}).join('')
+
+document.getElementById('container').innerHTML = itemsHtml
