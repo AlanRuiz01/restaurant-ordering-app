1d7e9045c1c0feb7a2cca61d9924d33e76ac3b90
Alan Ruiz <matiasruizc315@gmail.com>
2024-09-03 12:57:54 -0400

    A

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 import {menuArray} from '/data.js'
 
+// Renderizar el menu
 const itemsHtml = menuArray.map(food => {
     return `
     <section>
-    <div class="foods">
-        <i class="fa-solid fa-plus buy-icon"></i>
-        <p class="icon"> ${food.emoji} </p>
+        <div class="foods">
+            <p class="icon" id="food-images-${food.name}"> ${food.emoji} </p>
+            <i class="fa-solid fa-plus buy-icon" data-pizza="${food.price}"></i>
             <div class="details">
                 <p class="food-names">${food.name}</p>
                 <p class="food-ingredients">${food.ingredients}</p>
                 <p class="price">$${food.price}</p>
             </div>
-    </div>
+        </div>
     </section>
-    `;
-}).join('')
 
+
+    `;
+}).join('');
+
+// <section>
+// <h2>Your order</h2>
+//     <div>
+//         <p>Pizza</p>
+//         <p></p>
+//     </div>
+//  <p></p>
+// </section>
 document.getElementById('container').innerHTML = itemsHtml
+
+//Renderizar la orden
+
+ 
+
+
+// Obtener los items que se quieran agregar
+document.addEventListener('click', e => {
+    console.log(e.target.dataset.pizza)
+})
